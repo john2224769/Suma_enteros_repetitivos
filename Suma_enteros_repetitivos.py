66fa6b2879e78faee1762abeb4d69e29f9344461
john2224769 <john2224769@correo.uis.edu.co>
2022-09-29 09:46:59 -0500

    Actualizacion 1

---
+++ Suma_enteros_repetitivos.py
@@ -0,0 +1,13 @@
+# programa 25: Hallar la suma de los primeros 10 numeros enteros por medio de instrucciones repetitivas
+
+print("\n---------------------------------------------------------------------------")
+print("--------------Suma de los primeros n numeros enteros ------------------------")
+print("---------------------------------------------------------------------------\n")
+
+# input
+n=int(input("\n Ingrese la cantidad de enteros a sumar "))
+
+# processing
+i=1
+s=0
+
